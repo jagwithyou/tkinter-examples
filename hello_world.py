ea0927b522988507ca3a9cb8c90c53d064590eb6
explorewithjag <jagwithyou@gmail.com>
2020-04-03 20:03:51 +0530

    Hello World Script Added

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+from tkinter import *
+#instantiating the tkinter
+root = Tk()
+#instantiating the  leble of root instance
+txtLabel = Label(root, text = "Hello World")
+#packing the eble to the root 
+txtLabel.pack()
+#Showing the root
+root.mainloop()
